e81bff45b8c3a66209fa3f642fa1b802295b6c5e
Supermegatom <supermegatom@gmail.com>
2018-06-04 00:05:24 +0200

    consensus : tweaking nSuperblockStartBlock

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -82,7 +82,7 @@ public:
         consensus.nBudgetPaymentsWindowBlocks = 100;
         consensus.nBudgetProposalEstablishingTime = 60*60*24;//NOT USED
         consensus.nSuperblockCycle = 500;//16616; // ~(60*24*30)/2.6, actual number of blocks per month is 200700 / 12 = 16725
-        consensus.nSuperblockStartBlock = 3000000+(consensus.nSuperblockCycle*2); // 614820; // The block at which 12.1 goes live (end of final 12.0 budget cycle)
+        consensus.nSuperblockStartBlock = 3294560+1000; // 614820; // The block at which 12.1 goes live (end of final 12.0 budget cycle)
         consensus.nGovernanceMinQuorum = 10;
         consensus.nGovernanceFilterElements = 20000;
         consensus.nMasternodeMinimumConfirmations = 10;
