c163ffe3bc66e5ba6cbb52037bd7566e49237e85
Alex <a.adam@wewanttoknow.com>
2018-06-17 19:57:32 +0200

    masternodeconfig : create masternode.sample.conf instead of masternode.conf

--- src/masternodeconfig.cpp
+++ src/masternodeconfig.cpp
@@ -19,8 +19,12 @@ bool CMasternodeConfig::read(std::string& strErr) {
     boost::filesystem::path pathMasternodeConfigFile = GetMasternodeConfigFile();
     boost::filesystem::ifstream streamConfig(pathMasternodeConfigFile);
 
-    if (!streamConfig.good()) {
-        FILE* configFile = fopen(pathMasternodeConfigFile.string().c_str(), "a");
+    if (!streamConfig.good()) 
+    {
+        boost::filesystem::path pathSampleConfigFile("masternode.sample.conf");
+        if (!pathSampleConfigFile.is_complete()) pathSampleConfigFile = GetDataDir() / pathSampleConfigFile;
+
+        FILE* configFile = fopen(pathSampleConfigFile.string().c_str(), "a");
         if (configFile != NULL) {
             std::string strHeader = "# Masternode config file\n"
                           "# Format: alias IP:port masternodeprivkey collateral_output_txid collateral_output_index\n"
